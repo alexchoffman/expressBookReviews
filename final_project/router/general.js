df0478ed7b2ef33f1c1004d5c14fb02835e192cc
alexchoffman <alexchoffman@hotmail.com>
2023-10-26 21:15:24 -0400

    updates

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,16 +18,24 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn
+    res.send(books[isbn])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
+    let booksbyauthor = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["author"] === req.params.author) {
+        booksbyauthor.push({"isbn":isbn,
+                            "title":books[isbn]["title"],
+                            "reviews":books[isbn]["reviews"],
+                            "author":books[isbn]["author"]});
+      }
+    });
+    res.send(JSON.stringify({booksbyauthor}, null, 4));
+  });
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
